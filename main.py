1c08878ed60124107b9d385576858fa71966c438
21zhou <21zhouyun@gmail.com>
2015-02-15 10:49:03 -0600

    udpated memcache mechanism

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import webapp2
 import urllib
+import datetime
+from google.appengine.api import memcache
+from google.appengine.ext import db
 
 #append path to resolve the third party issue at google app engine
 import sys
@@ -7,33 +10,42 @@ sys.path.insert(0, 'libs')
 
 from feedgen.feed import FeedGenerator
 from bs4 import BeautifulSoup
+from pytz import timezone
 
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(generate_rss)
+        article = get_article("http://plato.stanford.edu/cgi-bin/encyclopedia/random")
+        self.response.headers['Cache-Control'] = 'public,max-age=%s' % 86400
+        self.response.headers['Content-Type'] = 'text/xml'
+        self.response.out.write(generate_rss(article))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
 ], debug=True)
 
 
-class Article:
-	def __init__(self, title, link, preamble, content):
-		self.title = title
-		self.link = link
-		self.preamble = preamble
-		self.content = content
+class Article(db.Model):
+		title = db.TextProperty(required=True)
+		link = db.StringProperty(required=True)
+		preamble = db.TextProperty(required=True)
+		content = db.TextProperty(required=True)
 
 def generate_rss(article):
 	fg = FeedGenerator()
 	#todo: change this to a real id later
-	fg.id('http://lernfunk.de/media/%s' % article.title)
+	fg.id('http://www.example.com')
+	fg.title("RESEP")
+	fg.link(href="http://plato.stanford.edu/", rel='self')
+	fg.description("This feed grabs a random entry from the Stanford Encyclopedia of Philosophy everyday")
 
-	fg.title(article.title)
-	fg.link(href=article.link)
-	fg.description("%s \n %s" % (article.preamble, article.content))
+	fe = fg.add_entry()
+	fe.title(article.title)
+	fe.link(href=article.link, rel='self')
+	fe.author(name="Stanford University")
+	fe.published(published=get_last_update_date())
+	fe.description("%s \n %s" % (article.preamble, article.content))
+	fe.content("%s \n %s" % (article.preamble, article.content))
 
 	fg.language('en')
 	rssfeed = fg.rss_str(pretty=True) # Get the RSS feed as string
@@ -41,18 +53,40 @@ def generate_rss(article):
 	#fg.rss_file('%s_rss.xml' % article.title) # Write the RSS feed to a file
 
 def get_article(url):
-	#create an url object
-	url_obj = urllib.urlopen(url)
-	soup = BeautifulSoup(url_obj.read())
-	title = soup.title.string
-	link = url_obj.geturl()
-	preamble = soup.find_all(id="preamble")[0].text
-	main_text = soup.find_all(id="main-text")[0].text
-	#contruct an article object
-	article = Article(title, link, preamble, main_text)
+	last_update = get_last_update_date()
+	cached_article_id = memcache.get('cached_article_id')
+	eastern = timezone('US/Eastern')
+	if (eastern.localize(datetime.datetime.now()) - last_update).days >= 1 or cached_article_id is None:
 
-	return article
+		#create an url object
+		url_obj = urllib.urlopen(url)
+		soup = BeautifulSoup(url_obj.read())
+		title = soup.title.string
+		link = url_obj.geturl()
+		preamble = soup.find_all(id="preamble")[0].text
+		content = soup.find_all(id="main-text")[0].text
+		#contruct an article object
+		article = Article(title=title, link=link, preamble=preamble, content=content)
+		article.put()
 
-#article = get_article("http://plato.stanford.edu/cgi-bin/encyclopedia/random")
+		memcache.add('cached_article_id', article.key().id())
+
+		return article
+
+	else:
+		return Article.get_by_id(cached_article_id)
+
+#deal with memcache
+def get_last_update_date():
+    date = memcache.get('last_update')
+    if date is not None:
+        return date
+    else:
+    	eastern = timezone('US/Eastern')
+        date = eastern.localize(datetime.datetime.now())
+        memcache.add('last_update', date)
+        return date
+
+
 #generate_rss(article)
 
